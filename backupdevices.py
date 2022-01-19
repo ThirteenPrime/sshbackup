31ed654c30e349b30437cf71e6191eb3229f0a5f
ThirteenPrime <benjhamoy@gmail.com>
2022-01-20 00:54:56 +0800

    moved getcredentials to function

--- backupdevices.py
+++ backupdevices.py
@@ -5,6 +5,7 @@ from netmiko import (
     ConnectHandler,
     NetmikoTimeoutException,
     NetmikoAuthenticationException,
+
 )
 
 
@@ -19,6 +20,8 @@ def send_show_command(device, command):
         return result
     except (NetmikoTimeoutException, NetmikoAuthenticationException) as error:
         print(error)
+    # except Exception as error:
+    #    print(error)
 
 
 def writetofile(inputtext='', hostname='CI', outputextension='txt'):
@@ -30,6 +33,7 @@ def writetofile(inputtext='', hostname='CI', outputextension='txt'):
     text_file = open(outputfile, "wt")
     text_file.write(inputtext)
     text_file.close()
+    print(f'writing file:{outputfile}')
     pass
 
 
@@ -38,17 +42,30 @@ command = 'show running-config'
 #  username: admin
 #  password: C1sco12345
 #  secret: cisco
-username = input('username: ')
-password = getpass.getpass('password: ')
-secretpass = getpass.getpass('secret password: ')
 
 
+def getcredentials_todict():
+    username = input('username: ')
+    password = getpass.getpass('password: ')
+    secretpass = getpass.getpass('secret password: ')
+    result = {'username': username,
+              'password': password, 'secret': secretpass}
+    return result
+
+
+credentialsdict = getcredentials_todict()
+failed_devices = []
 with open("devices.yaml") as f:
     devices = yaml.safe_load(f)
 for device in devices:
-    device['username'] = username
-    device['password'] = password
-    device['secret'] = secretpass
+    device = {**device, **credentialsdict}
     result = send_show_command(device, command)
     # result commands = {'command':'output'}
-    writetofile(result, device['host'])
+    if result:
+        writetofile(result, device['host'])
+    else:
+        failed_devices.append(device)
+print(f'failed devices:')
+#print(x['host'] for x in failed_devices)
+for x in failed_devices:
+    print(x['host'])
